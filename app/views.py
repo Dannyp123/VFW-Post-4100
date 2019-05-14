8725c498d7750d13be5adce2e385dba53a88768f
Daniel Peterson <dpeterson@basecampcodingacademy.org>
2019-05-14 13:50:52 -0500

    added login and signing up to the app.

--- app/views.py
+++ app/views.py
@@ -3,9 +3,28 @@ from django.views import View
 from app import forms
 from app import models
 from django.shortcuts import redirect, render
+from .forms import UserRegisterForm
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 
 #view for home page
 class HomePage(View):
     def get(self, request):
+        messages.success(request, f"Welcome!")
         return render(request, "home.html")
+
+
+def register(request):
+    if request.method == "POST":
+        form = UserRegisterForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get("username")
+            messages.success(
+                request, f"Your account has been created. You can now login")
+            return redirect("login")
+    else:
+        form = UserRegisterForm()
+
+    return render(request, "sign-up.html", {"form": form})
--- vfwpost4100/urls.py
+++ vfwpost4100/urls.py
@@ -16,8 +16,18 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from app import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.HomePage.as_view(), name="home"),
+    path("signup/", views.register, name="sign-up"),
+        path(
+        "login/",
+        auth_views.LoginView.as_view(template_name="login.html"),
+        name="login"),
+    path(
+        "logout/",
+        auth_views.LogoutView.as_view(template_name="logout.html"),
+name="logout"),
 ]
